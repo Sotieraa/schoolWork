87b14fd91f393611c8e494e75b47dd21afa87f36
Sotier <109980374+Sotieraa@users.noreply.github.com>
2022-10-06 21:40:01 +0800

    Add files via upload

---
+++ cppProgram/work/5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int g;
+    cin >> g;
+    if(g>=90)
+    cout<<"A"<<endl;
+    else if(g>=80)
+    cout<<"B"<<endl;
+    else if(g>=70)
+    cout<<"C"<<endl;
+    else if(g>=60)
+    cout<<"D"<<endl;
+    else cout<<"E"<<endl;
+    return 0;
+}
